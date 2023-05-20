97c73d50c34a12d2d2689e9f9b67c24ffbe31b0f
AbdullahFathan <abdullahfathan013@gmail.com>
2023-05-20 09:04:45 +0700

    feat: simple layout  navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,47 @@
 import styles from "./style";
 
+import {
+  Navbar,
+  Hero,
+  Billing,
+  Business,
+  Button,
+  CTA,
+  CardDeal,
+  Client,
+  Testimoni,
+  Stats,
+  FeedbackCard,
+  Footer,
+} from "./components";
+
 const App = () => {
   return (
     //Navbar
     <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>NavBar</div>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
       </div>
       {/* Hero Section */}
       <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>Hero</div>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
       </div>
 
       {/*Stats Section */}
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Stats Bussness Billing CardDeal Testimoni Client CTA Footer
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimoni />
+          <Client />
+          <CTA />
+          <Footer />
         </div>
       </div>
     </div>
---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Hero from "./Hero";
+import Billing from "./Billing";
+import Business from "./Business";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+import Client from "./Client";
+import CTA from "./CTA";
+import FeedbackCard from "./FeedbackCard";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Navbar from "./Navbar";
+import Stats from "./Stats";
+import Testimoni from "./Testimoni";
+
+export {
+  Hero,
+  Billing,
+  Business,
+  Button,
+  CardDeal,
+  Client,
+  CTA,
+  FeedbackCard,
+  Footer,
+  GetStarted,
+  Navbar,
+  Stats,
+  Testimoni,
+};
