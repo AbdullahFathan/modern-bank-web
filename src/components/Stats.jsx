67bd19611d8c67511f9f8c1250e0ff24aeeb836a
AbdullahFathan <abdullahfathan013@gmail.com>
2023-05-22 16:19:47 +0700

    style: business section done

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -4,7 +4,7 @@ import styles from "../style";
 const Stats = () => {
   return (
     <section
-      className={`${styles.flexCenter} sm:flex-row flex-col flex-warp sm:mb-20 mb-6`}
+      className={`${styles.flexCenter} flex-row  flex-wrap sm:mb-20 mb-6`}
     >
       {stats.map((stat) => (
         <div
